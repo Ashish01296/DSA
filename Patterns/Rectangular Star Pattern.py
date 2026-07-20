ab00e9ab5e2b4b25c227bac3bc31db61e022df01
Ashish01296 <ashishvinod160@gmail.com>
2026-07-20 19:03:40 +0530

    learning Patterns

---
+++ Patterns/Rectangular Star Pattern.py
@@ -0,0 +1,14 @@
+"""
+
+*****
+*****
+*****
+*****
+*****
+
+"""
+number = int(input("Enter a number: "))
+for i in range(number):
+    for j in range(number):
+        print("*",end="")
+    print()
